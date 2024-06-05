ddd970547da7087c48f255793137ec2db4f5950f
Phuc Nghi <meocodien1762003@gmail.com>
2024-06-05 22:22:49 +0700

    testting

---
+++ ThrivoHR.API/Controllers/Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThrivoHR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        //test
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
